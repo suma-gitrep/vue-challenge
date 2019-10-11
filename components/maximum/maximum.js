e8b8dee496f3e59d86a7b07e2c45a066e2cb6afc
suma-gitrep <s537239@nwmissouri.edu>
2019-10-11 11:27:46 -0500

    made some changes

--- components/maximum/maximum.js
+++ components/maximum/maximum.js
@@ -5,14 +5,14 @@ const maxi = (x, y) => {
 const subtracter = new Vue({
   el: '#maximum',
   data: {
-    first: 6,
-    second: 2
+    one: 6,
+    two: 2
   },
   computed: {
     maximumnum: function () {
-      const i = parseInt(this.first)
-      const j = parseInt(this.second)
-      return `The maximum of ${this.first} and ${this.second} is ${maxi(i, j)}.`
+      const i = parseInt(this.one)
+      const j = parseInt(this.two)
+      return `The maximum of ${this.one} and ${this.two} is ${maxi(i, j)}.`
     }
   }
 })
